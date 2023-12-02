77501c53515266155db229479fbe1f857a9bc33c
dimasasharw <dimas.ashar1997@gmail.com>
2023-12-02 11:31:30 +0700

    finalize-logout

--- react-tmdb/src/components/Navbar.jsx
+++ react-tmdb/src/components/Navbar.jsx
@@ -1,14 +1,16 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "../style/navbar.css";
 
+
 const Navbar = () => {
+  const navigate = useNavigate()
   const sessionId = localStorage.getItem("sessionId");
 
   const handleLogout = () => {
     localStorage.removeItem("sessionId");
     localStorage.removeItem("accountId");
-
+    navigate('/')
     window.location.reload();
   };
 
